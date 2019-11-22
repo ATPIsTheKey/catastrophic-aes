239c2b0e2bb2c3b14afa883b7c56ad8e8832fbd5
ATPIsTheKey <roland.fmn@gmail.com>
2019-11-22 20:53:18 +0200

    work transfer

--- main.c
+++ main.c
@@ -5,77 +5,245 @@
 
 #include <openssl/evp.h>
 #include <openssl/rand.h>
+#include <regex.h>
 
 #include "libs/argtable3.h"
 #include "tests/src/aes_quicktest.h"
 #include "catastrophic-aes/utils.h"
 
+#define VERSION "1.0"
 
-/* global arg_xxx structs */
-struct arg_lit *verb, *help, *version, *opmode;
+/* SYNTAX 1: encrypt */
+struct arg_rex *cmd1;
+struct arg_lit *opmode, *verb1, *help1, *vers1;
 struct arg_int *klen;
-struct arg_file *o, *file;
-struct arg_end *end;
+struct arg_file *o1, *file1, *fkey1;
+struct arg_end *end1;
+int nerrors1;
+
+/* SYNTAX 2: decrypt */
+struct arg_rex *cmd2;
+struct arg_lit *verb2, *help2, *vers2;
+struct arg_file *o2, *file2, *fkey2;
+struct arg_end *end2;
+int nerrors2;
+
+/* SYNTAX 3: help or version*/
+struct arg_lit *help3;
+struct arg_lit *vers3;
+struct arg_end *end3;
+int nerrors3;
+
+int
+mainprocedure_encryption()
+{
+    return 0;
+}
+
+int
+mainprocedure_decryption()
+{
+    return 0;
+}
+
+int
+mainprocedure_help(int help, int version, const char *progname,
+                   void *argtable1, void *argtable2, void *argtable3)
+{
+    if (help) {
+        printf("Usage: %s", progname);
+        arg_print_syntax(stdout, argtable1, "\n");
+        printf("       %s", progname);
+        arg_print_syntax(stdout, argtable2, "\n");
+        printf("       %s", progname);
+        arg_print_syntax(stdout, argtable3, "\n");
+        printf("       %s", progname);
+        printf("This program encrypts/decrypts files by the Advanced "
+               "Encryption Standard.\n");
+        arg_print_glossary(stdout, argtable1, "      %-20s %s\n");
+        arg_print_glossary(stdout, argtable2, "      %-20s %s\n");
+        arg_print_glossary(stdout, argtable3, "      %-20s %s\n");
+    }
+
+    if (version)
+    {
+        printf("%s version "VERSION, progname);
+        printf("There is NO warranty; not even for MERCHANTABILITY or "
+               "FITNESS FOR A PARTICULAR PURPOSE.");
+        return 0;
+    }
+    printf("Try '%s --help' for more information.\n", progname);
+
+    return 0;
+}
+
 
 int
 main(int argc, char **argv)
 {
-    /*** Initialize arg parser *****************************************************/
+    /*** Initialize arg parser ************************************************/
 
-    /* the global arg_xxx structs are initialised within the argtable */
-    void *argtable[] = {
-            verb    = arg_litn("v", "verbose", 0, 1, "verbose output"),
-            help    = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
-            version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
-            o       = arg_filen("o", NULL, "myfile", 1, 1, "output file"),
-            file    = arg_filen(NULL, NULL, "<file>", 1, 1, "input files"),
-            klen    = arg_intn("k", "klen", "<n>", 0, 1, "AES key length of either 128, 192 or 256; default is 128"),
-            opmode  = arg_litn("c", "mode", 0, 1, "set block cipher mode of operation; default is CBC"),
-            end     = arg_end(20),
+    /// Argtable 1 for encryption command //////////////////////////////////////
+
+    void *argtable1[] = {
+        cmd1   = arg_rex1(
+                NULL, NULL, "encrypt", NULL, REG_ICASE,
+                "encrypt a file"
+        ),
+
+        verb1  = arg_litn(
+                "v", "verbose", 0, 1,
+                "verbose output"
+        ),
+
+        help1  = arg_litn(
+                NULL, "help", 0, 1,
+                "display this help and exit"
+        ),
+
+        vers1  = arg_litn(
+                NULL, "version", 0, 1,
+                "display version info and exit"
+        ),
+
+        o1      = arg_filen(
+                "o", NULL, "<file>", 1, 1,
+                "output encrypted file"
+        ),
+
+        file1   = arg_filen(
+                NULL, NULL, "<file>", 1, 1,
+                "input file to be encrypted"
+        ),
+
+        fkey2   = arg_filen(
+                NULL, NULL, "<file>", 0, 1,
+                "output for file key (optional only)"
+        ),
+
+        klen = arg_intn(
+                "l", "klen", "<int>", 0, 1,
+                "key length of either 128, 192 or 256 (default is 128)"
+        ),
+
+        end1   = arg_end(9)
     };
 
+    /// Argtable 2 for decryption command //////////////////////////////////////
+
+    void *argtable2[] = {
+            cmd2   = arg_rex1(
+                    NULL, NULL, "decrypt", NULL, REG_ICASE,
+                    "decrypt a file"
+            ),
+
+            verb2  = arg_litn(
+                    "v", "verbose", 0, 1,
+                    "verbose output"
+            ),
+
+            help2  = arg_litn(
+                    NULL, "help", 0, 1,
+                    "display this help and exit"
+            ),
+
+            vers2  = arg_litn(
+                    NULL, "version", 0, 1,
+                    "display version info and exit"
+            ),
+
+            o2      = arg_filen(
+                    "o", NULL, "myfile", 1, 1,
+                    "output decrypted file"
+            ),
+
+            file2   = arg_filen(
+                    NULL, NULL, "<file>", 1, 1,
+                    "input encrypted file"
+            ),
+
+            fkey2    = arg_filen(
+                    "k", NULL, "<file>", 1, 1,
+                    "file containing key"
+            ),
+
+            end2   = arg_end(8)
+    };
+
+    void *argtable3[] = {
+            help3 = arg_lit0(
+                    NULL,"help",
+                    "print this help and exit"
+            ),
+
+            vers3 = arg_lit0(
+                    NULL,"version",
+                    "print version information and exit"
+            ),
+
+            end2   = arg_end(20)
+    };
+
+    char progname[] = "catastrophic-aes";
     int exitcode = 0;
-    char progname[] = "catastrophicaes";
 
-    int nerrors;
-    nerrors = arg_parse(argc,argv,argtable);
-
-    /* special case: '--help' takes precedence over error reporting */
-    if (help->count > 0)
+    if (arg_nullcheck(argtable1)!=0 ||
+        arg_nullcheck(argtable2)!=0 ||
+        arg_nullcheck(argtable3)!=0)
     {
-        printf("Usage: %s", progname);
-        arg_print_syntax(stdout, argtable, "\n");
-        printf("Demonstrate command-line parsing in argtable3.\n\n");
-        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
-        exitcode = 0;
+        /* NULL entries were detected, some allocations must have failed */
+        printf("%s: insufficient memory\n",progname);
+        exitcode=1;
         goto exit;
     }
 
-    /* If the parser returned any errors then display them and exit */
-    if (nerrors > 0)
+    /* Above we defined a separate argtable for each possible command line syntax */
+    /* and here we parse each one in turn to see if any of them are successful    */
+    nerrors1 = arg_parse(argc, argv, argtable1);
+    nerrors2 = arg_parse(argc, argv, argtable2);
+    nerrors3 = arg_parse(argc, argv, argtable3);
+
+
+    if (nerrors1 == 0)
+        exitcode = mainprocedure_encryption(
+                verb1->count, help1->count, vers1->count);
+    else if (nerrors2 == 0)
+        exitcode = mainprocedure_decryption();
+    else if (nerrors3)
+        exitcode = mainprocedure_help()
+
+    if (cmd1->count > 0)
     {
-        /* Display the error details contained in the arg_end struct.*/
-        arg_print_errors(stdout, end, progname);
-        printf("Try '%s --help' for more information.\n", progname);
-        exitcode = 1;
-        goto exit;
+        /* encryption args correct, so presume decryption syntax was intended
+         * target */
+        arg_print_errors(stdout, end1, progname);
+        printf("usage: %s ", progname);
+        arg_print_syntax(stdout, argtable1,"\n");
     }
-
-    /*** begin encryption procedure ************************************************/
-
-    char full_path_in[PATH_MAX];
-    char full_path_out[PATH_MAX];
-    sprintf(full_path_in, "%s", file->filename[0]);
-    sprintf(full_path_out, "%s.crypt", o->filename[0]);
-
-    FILE *fp_in   = fopen(full_path_in, "rb");
-    FILE *fp_out  = fopen(full_path_out, "wb+");
-//    FILE *key_out = fopen("")
-
-    pw_input_s *pw = input_pw(stdin, 32);
+    else if (cmd2->count > 0)
+    {
+        /* decrpytion args correct, so presume decryption syntax was intended
+         * target */
+        arg_print_errors(stdout, end2, progname);
+        printf("usage: %s ", progname);
+        arg_print_syntax(stdout, argtable2, "\n");
+    }
+    else
+    {
+        /* no correct cmd literals were given */
+        printf("%s: missing encryption | decryption | help command.\n",
+                progname);
+        printf("usage 1: %s ", progname);
+        arg_print_syntax(stdout, argtable1, "\n");
+        printf("usage 2: %s ", progname);
+        arg_print_syntax(stdout, argtable2, "\n");
+        printf("usage 3: %s ", progname);
+        arg_print_syntax(stdout, argtable3, "\n");
+    }
 
     exit:
     /* deallocate each non-null entry in argtable[] */
-    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    arg_freetable(argtable1, sizeof(argtable1) / sizeof(argtable1[0]));
+    arg_freetable(argtable2, sizeof(argtable2) / sizeof(argtable2[0]));
     return exitcode;
 }
